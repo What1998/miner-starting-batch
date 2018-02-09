7a658defd80a5dad7ba06469b96e5e17fc746980
What1998 <33244001+What1998@users.noreply.github.com>
2018-02-08 18:20:30 -0600

    Add files via upload

--- etnstarter.bat
+++ etnstarter.bat
@@ -1,13 +1,12 @@
 @echo off
 :loop
-	cd "D:\mining\mining3\Claymore\Claymore"
+	cd "D:\mining\mining3\Claymore\Claymore" REM enter pathway
 	start start_etn
-	echo press t to end.
 	timeout /t 43200 REM 12 hrs in seconds
 	set /P t=[t]
 	IF "t"=="t" GOTO :end
-	taskkill /F /T /IM NsGpuCNMiner.exe REM force kills miner
+	taskkill /F /T /IM NsGpuCNMiner.exe REM enter process name found in taskmanager --force kills miner
 	timeout /t 3600 REM 1 hr in seconds
 goto loop
 :end
-	taskkill /F /T /IM NsGpuCNMiner.exe
+	taskkill /F /T /IM NsGpuCNMiner.exe REM enter process name found in taskmanager --force kills miner
