ba63e46e6fc721e84b0646e7f47455ccb4ec9279
What1998 <33244001+What1998@users.noreply.github.com>
2018-02-08 21:17:51 -0600

    Add files via upload

--- etnstarter.bat
+++ etnstarter.bat
@@ -1,12 +1,33 @@
 @echo off
 :loop
-	cd "D:\mining\mining3\Claymore\Claymore" REM enter pathway
-	start start_etn
-	timeout /t 43200 REM 12 hrs in seconds
-	set /P t=[t]
-	IF "t"=="t" GOTO :end
-	taskkill /F /T /IM NsGpuCNMiner.exe REM enter process name found in taskmanager --force kills miner
-	timeout /t 3600 REM 1 hr in seconds
+	set option=
+	set /p option=Type 1 to end.
+	if '%option%'=='1' goto end
+	if not '%option%'=='1' goto ready
+	:ready
+	cd "D:\mining\mining3\Claymore\Claymore" 
+	REM enter pathway
+	start startetn
+	echo [y]kill or [s]skip choice
+	choice /c ys /t 9998 /d s 
+		if %errorlevel%== 1 goto end
+	choice /c ys /t 9998 /d s
+		if %errorlevel%== 1 goto end
+	choice /c ys /t 9998 /d s
+		if %errorlevel%== 1 goto end
+	choice /c ys /t 9998 /d s
+		if %errorlevel%== 1 goto end
+	choice /c ys /t 3208 /d s
+		if %errorlevel%== 1 goto end
+	REM 12 hrs in seconds
+	taskkill /F /T /IM NsGpuCNMiner.exe 
+	REM enter process name found in taskmanager --force kills miner
+	echo [y]kill or [s]kip
+	choice /c ys /t 3600 /d s 
+		if %errorlevel%== 1 goto end
+	REM 1 hr in seconds
+
 goto loop
 :end
-	taskkill /F /T /IM NsGpuCNMiner.exe REM enter process name found in taskmanager --force kills miner
+	taskkill /F /T /IM NsGpuCNMiner.exe 
+	REM enter process name found in taskmanager --force kills miner
